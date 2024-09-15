3cdc10bdc099743b4bbcf24a0928ec836e37cb79
Vladimir Markelov <vsmarkelov@yandex.ru>
2024-09-15 23:38:18 +0300

    feat: функция создания карточки

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,19 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
+const cardContainer = document.querySelector('.places__list');
 
 // @todo: DOM узлы
 
 // @todo: Функция создания карточки
+function addCard(name, link) {
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+
+  cardElement.querySelector('.card__title').textContent = name;
+  cardElement.querySelector('.card__image').src = link;
+  cardContainer.append(cardElement);
+}
 
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
