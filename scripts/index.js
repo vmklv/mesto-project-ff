491caf45ca81b755794a73c82c16e8f8433a133f
Vladimir Markelov <vsmarkelov@yandex.ru>
2024-09-16 13:51:10 +0300

    feat: Функция удаления карточки

--- scripts/index.js
+++ scripts/index.js
@@ -2,22 +2,24 @@
 const cardTemplate = document.querySelector('#card-template').content;
 const cardContainer = document.querySelector('.places__list');
 
-
-// @todo: DOM узлы
-
 // @todo: Функция создания карточки
 function addCard(name, link) {
   const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
 
   cardElement.querySelector('.card__title').textContent = name;
   cardElement.querySelector('.card__image').src = link;
+
+  // @todo: Функция удаления карточки
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+  deleteButton.addEventListener('click', function () {
+    const listItem = deleteButton.closest('.card');
+    listItem.remove();
+  });
+
   cardContainer.append(cardElement);
 }
 
-// @todo: Функция удаления карточки
-
 // @todo: Вывести карточки на страницу
-
 initialCards.forEach(card => {
   addCard(card.name, card.link);
-})
+});
